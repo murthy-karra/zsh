6ef361c71fb5b1d2b7f4a31c55800ce3095f9c79
Murthy Karra <murthy.karra@gmail.com>
2025-10-31 23:20:16 -0700

    Added runme.sh ohmyzsh

--- runme.sh
+++ runme.sh
@@ -2,5 +2,9 @@
 
 sudo apt update && sudo apt upgrade -Y
 
-sudo apt install zsh
+sudo apt install zsh -Y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
 
